3441cfdd16c95be5343a63a51d7b478dafabb58a
Luis Sequitin <71285960+jerichosequitin@users.noreply.github.com>
2022-11-09 23:10:06 +0800

    Update PrescriptionController.php

--- app/Http/Controllers/PrescriptionController.php
+++ app/Http/Controllers/PrescriptionController.php
@@ -10,12 +10,7 @@ class PrescriptionController extends Controller
 {
     public function index(User $user)
     {
-        $doc = DB::table('users')
-            ->join('doctor_profiles','users.id','=','doctor_profiles.user_id')
-            ->where('type', '=', 'doctor')
-            ->where('isVerified', '=', 'Enabled')
-            ->get();
-        return view ('prescription')->with('doc', $doc);
+        return view ('prescription');
     }
 
 }
