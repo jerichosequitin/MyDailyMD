61b49eb67c99610bbeec6d10ac45a5e675f93dfb
Katrina Belardo <katrinafrances.belardo@benilde.edu.ph>
2022-12-07 00:29:15 +0800

    mail controller + mail view blade

---
+++ resources/views/notification.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>MyDailyMD E-Prescription</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container text-center m-1">
+    <h1 class="lead mt-3">Hello there, patient! Here is a copy of your e-prescription.</h1>
+    <p class="lead">Please check attachment with this file.</p>
+</div>
+</body>
+</html>
